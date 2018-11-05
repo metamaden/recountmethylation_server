9abdd51d3a5c132150841e56609f67f3c1fd8e06
Abhi Nellore <anellore@gmail.com>
2018-11-05 14:39:49 -0800

    adds retries to dl function

--- scratch/dl_idat.py
+++ scratch/dl_idat.py
@@ -3,73 +3,84 @@ import datetime
 import os
 import subprocess
 
-def dl_idat(input_list,dldir):
-    """
-        Download idats, 
+def dl_idat(input_list, dldir, retries=3, interval=.1):
+    """ Download idats, 
         Reads in either list of GSM IDs or ftp addresses
     
         Arguments
             * input list : list of GSM IDs
             * dldir : target directory for new downloads
+            * retries: number of times to retry a given file to download
+            * interval: time (in s) before first retry
 
         Returns 
             * Dictionary showing records, dates, and exit statuses of ftp calls
+            OR error string over connection issues
     """
     item = input_list[0]
     if not item.startswith('GSM'):
         raise RuntimeError("GSM IDs must begin with \"GSM\".")
     ftptoken_login = 'ftp.ncbi.nlm.nih.gov'
-    if not os.path.exists(dldir):
-        subprocess.call('mkdir '+dldir, shell=True)
+    os.makedirs(dldir, exist_ok=True)
     try:
         ftp = ftplib.FTP(ftptoken_login)
         loginstat = ftp.login()
     except ftplib.all_errors as e:
         return str(e)
-    
     dldict = {}
-    for id in input_list:
-        dldict[id] = []
-        id_ftptokens = ['ftp.ncbi.nlm.nih.gov','geo','samples',
-        id[:-3]+'nnn',id,'suppl']
+    for gsm_id in input_list:
+        dldict[gsm_id] = []
+        id_ftptokens = [
+                'ftp.ncbi.nlm.nih.gov', 'geo', 'samples',
+                gsm_id[:-3] + 'nnn', gsm_id, 'suppl'
+            ]
         id_ftpadd = '/'.join(id_ftptokens[1::])+'/'
-        try:
-            filenames = ftp.nlst(id_ftpadd)
-            dldict[id].append([id,
-                id_ftpadd,
-                "success"]
-                ) 
-            for file in filenames:
-                filedate = ""
-                filedate_estat = ""
-                filedl_estat = ""
-                file_tokens = file.split('/')
-                try:
-                    filedate = ftp.sendcmd("MDTM /" + '/'.join(file_tokens))
-                    filedate = datetime.datetime.strptime(filedate[4:],
-                        "%Y%m%d%H%M%S")
-                    filedate_estat = "success"
-                except ftplib.all_errors as efiledate:
-                    filedate_estat = str(efiledate)
-                    filedate = "not_available"
-                filename_towrite = file_tokens[-1]
-                file_ftpadd = '/'.join(file_tokens[:-1])
-                file_ftpadd = file_ftpadd+'/'+file_tokens[-1:][0]
-                try:
-                    filedl_estat = ftp.retrbinary("RETR /"+file_ftpadd,
-                        open(os.path.join(dldir,
-                            filename_towrite),'wb').write
-                        )
-                except ftplib.all_errors as efiledl:
-                    filedl_estat = str(efiledl)
-                dldict[id].append(
-                    [id,
-                    filename_towrite,
-                    file_ftpadd,
-                    filedate,
-                    filedate_estat,
-                    filedl_estat]
+        retries_left = retries
+        while retries_left:
+            try:
+                filenames = ftp.nlst(id_ftpadd)
+                dldict[gsm_id].append([gsm_id,
+                    id_ftpadd,
+                    "success"]
+                    ) 
+                for file in filenames:
+                    filedate = ""
+                    filedate_estat = ""
+                    filedl_estat = ""
+                    file_tokens = file.split('/')
+                    try:
+                        filedate = ftp.sendcmd("MDTM /" + '/'.join(file_tokens))
+                        filedate = datetime.datetime.strptime(filedate[4:],
+                            "%Y%m%d%H%M%S")
+                        filedate_estat = "success"
+                    except ftplib.all_errors as efiledate:
+                        filedate_estat = str(efiledate)
+                        filedate = "not_available"
+                    to_write = file_tokens[-1]
+                    file_ftpadd = '/'.join(file_tokens[:-1])
+                    file_ftpadd = file_ftpadd+'/'+file_tokens[-1:][0]
+                    try:
+                        with open(
+                                os.path.join(dldir, to_write), 'wb'
+                            ) as output_stream:
+                            filedl_estat = ftp.retrbinary(
+                                    "RETR /"+file_ftpadd,
+                                    output_stream.write
+                                )
+                    except ftplib.all_errors as efiledl:
+                        filedl_estat = str(efiledl)
+                    dldict[gsm_id].append(
+                        [gsm_id,
+                        filename_towrite,
+                        file_ftpadd,
+                        filedate,
+                        filedate_estat,
+                        filedl_estat]
                     )
-        except ftplib.error_temp as eid:
-            dldict[id].append([id,id_ftpadd,str(eid)])
+            except ftplib.error_temp as eid:
+                if retries_left:
+                    retries_left -= 1
+                    time.sleep(interval)
+                    continue
+                dldict[gsm_id].append([gsm_id, id_ftpadd, str(eid)])
     return dldict
