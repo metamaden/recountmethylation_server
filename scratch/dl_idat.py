0fdd3460dc7028682fe3420a9e6d4fa9e340a9c9
Abhi Nellore <anellore@gmail.com>
2018-11-05 15:43:51 -0800

    adds check for whether downloaded files are same as previous version

--- scratch/dl_idat.py
+++ scratch/dl_idat.py
@@ -2,8 +2,26 @@ import ftplib
 import datetime
 import os
 import subprocess
+import glob
+import filecmp
 
-def dl_idat(input_list, dldir, retries=3, interval=.1):
+def gettime_ntp(addr='time.nist.gov'):
+    # http://code.activestate.com/recipes/117211-simple-very-sntp-client/
+    import socket
+    import struct
+    import sys
+    import time
+    TIME1970 = 2208988800L      # Thanks to F.Lundh
+    client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    data = '\x1b' + 47 * '\0'
+    client.sendto(data, (addr, 123))
+    data, address = client.recvfrom( 1024 )
+    if data:
+        t = struct.unpack( '!12I', data )[10]
+        t -= TIME1970
+    return t
+
+def dl_idat(input_list, dldir, retries=3, interval=.1, validate=True):
     """ Download idats, 
         Reads in either list of GSM IDs or ftp addresses
     
@@ -12,11 +30,16 @@ def dl_idat(input_list, dldir, retries=3, interval=.1):
             * dldir : target directory for new downloads
             * retries: number of times to retry a given file to download
             * interval: time (in s) before first retry
+            * validate: compare most recently downloaded version with previous
+                version of file after download and if new file is same, delete
+                it and make note in dictionary
+            ADD db connector to be able to make query in comment
 
         Returns 
             * Dictionary showing records, dates, and exit statuses of ftp calls
             OR error string over connection issues
     """
+    timestamp = str(gettime_ntp())
     item = input_list[0]
     if not item.startswith('GSM'):
         raise RuntimeError("GSM IDs must begin with \"GSM\".")
@@ -36,6 +59,7 @@ def dl_idat(input_list, dldir, retries=3, interval=.1):
             ]
         id_ftpadd = '/'.join(id_ftptokens[1::])+'/'
         retries_left = retries
+        files_written = []
         while retries_left:
             try:
                 filenames = ftp.nlst(id_ftpadd)
@@ -52,11 +76,14 @@ def dl_idat(input_list, dldir, retries=3, interval=.1):
                         filedate = ftp.sendcmd("MDTM /" + '/'.join(file_tokens))
                         filedate = datetime.datetime.strptime(filedate[4:],
                             "%Y%m%d%H%M%S")
+                        # Check if filedate is the same as in db, and if it is
+                        # break out, or indicate in return value that this was
+                        # the case.
                         filedate_estat = "success"
                     except ftplib.all_errors as efiledate:
                         filedate_estat = str(efiledate)
                         filedate = "not_available"
-                    to_write = file_tokens[-1]
+                    to_write = '.'.join([gsm_id, timestamp, file_tokens[-1]])
                     file_ftpadd = '/'.join(file_tokens[:-1])
                     file_ftpadd = file_ftpadd+'/'+file_tokens[-1:][0]
                     try:
@@ -71,16 +98,37 @@ def dl_idat(input_list, dldir, retries=3, interval=.1):
                         filedl_estat = str(efiledl)
                     dldict[gsm_id].append(
                         [gsm_id,
-                        filename_towrite,
+                        to_write,
                         file_ftpadd,
                         filedate,
                         filedate_estat,
                         filedl_estat]
                     )
+                    if '226 Transfer complete' in filedl_estat:
+                        files_written.append(
+                                (gsm_id, to_write, len(dldict[gsm_id]) - 1)
+                            )
             except ftplib.error_temp as eid:
                 if retries_left:
                     retries_left -= 1
                     time.sleep(interval)
                     continue
                 dldict[gsm_id].append([gsm_id, id_ftpadd, str(eid)])
+            else:
+                break
+        if validate:
+            for gsm_id, file_written, index in files_written:
+                gsms = glob.glob('.'.join([
+                            os.path.join(dldir, gsm_id), '*', '.'.join(
+                                            file_written.split('.')[2:]
+                                    )
+                        ]))
+                most_recent = gsms.sort(
+                        key=lambda x: int(x.split('.')[1])
+                    )[-2:]
+                if len(most_recent) == 2:
+                    if filecmp.cmp(most_recent[0], most_recent[1]):
+                        os.remove(most_recent[1])
+                        # If filename is false, we found it was the same
+                        dldict[gsm_id][index][1] = False
     return dldict
