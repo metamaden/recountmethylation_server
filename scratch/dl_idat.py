a4543dfb24cb7e16a98996d02c41a5d21b043f43
Sean Maden <metamaden@users.noreply.github.com>
2018-11-07 21:11:06 -0800

    fix typo, grn to red
    Fix typo in mongodb function for red subcollection.

--- scratch/dl_idat.py
+++ scratch/dl_idat.py
@@ -63,7 +63,7 @@ def connect_mongo(gsm_id,filename,hostname='localhost',conport=27017):
                         return_status_list.append('subcollection \'red\' not found')
                     else:
                         if idatsc.red.find_one({'__id': gsm_id}):
-                            db_query.append(grn.find_one({'__id': gsm_id}))
+                            db_query.append(red.find_one({'__id': gsm_id}))
                         else:
                             return_status.append('gsm record not in subcollection')
     return [return_status,db_query]
