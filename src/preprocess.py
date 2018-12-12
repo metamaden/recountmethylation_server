ca6a71c0621da712aad3e48acda14494c2bf8c48
Sean <maden@ohsu.edu>
2018-12-11 17:46:17 -0800

    towards soft file processing
    Fixes up some issues with processing SOFT files into GSM JSON.

--- src/preprocess.py
+++ src/preprocess.py
@@ -4,6 +4,18 @@ import sys
 import os
 import datetime
 import inspect
+import re
+sys.path.insert(0, os.path.join("recount-methylation-server","src"))
+from process_soft import expand_soft,extract_gsm_soft,gsm_soft2json
+from process_soft import msrap_prepare_json,run_metasrapipeline
+from server import get_queryfilt_dict
+
+""" preprocess.py
+    Various wrappers to preprocessing utility functions.
+    Prepares SOFT data for by-GSM input to MetaSRA-pipeline.
+    Returns lists of existant SOFT and idat files to process.
+
+"""
 
 def get_mongofiles_for_preprocessing(idatsdir,softdir):
     """ Get GSE and GSM IDs from MongoDB 
@@ -20,9 +32,11 @@ def get_mongofiles_for_preprocessing(idatsdir,softdir):
     csoft = client.recount_methylation.gse.soft
     # grab unique gsm ids
     idatslist = list(client.recount_methylation.gsm.idats.find())
+    idatslist = [record for record in idatslist if 'gsmid' in record.keys()]
     gsmindex = list(set([record['gsmid'] for record in idatslist]))
     # grab unique gse ids
     softlist = list(client.recount_methylation.gse.soft.find())
+    softlist = [record for record in softlist if 'gseid' in record.keys()]
     gseindex = set([record['gseid'] for record in softlist])
         # filter all records for gsm on most recent update datetime
     idatrecordsfilt = {}
@@ -67,7 +81,8 @@ def get_mongofiles_for_preprocessing(idatsdir,softdir):
         else:
             idatrecordsfilt[gsm].append('missingidat')
     # grab and filter soft file list
-    softrecordsfilt = []
+    softrecordsfilt = {}
+    print(softrecordsfilt)
     for gse in gseindex:
         softrecordsfilt[gse] = []
         gsesoftlist = [record for record in recordsgsm 
@@ -97,3 +112,91 @@ def get_mongofiles_for_preprocessing(idatsdir,softdir):
     drfiles['soft'] = softrecordsfilt
     return drfiles
 
+def preprocess_softlist(softlist,
+    filesdir = 'recount-methylation-files',
+    softdir = 'gse_soft',
+    expand_soft = True, extract_gsm = True, conv_json = True, 
+    msrap_preprare = True):
+    """ Runs preprocessing on soft files
+        Arguments
+            * softdir: directory to look for soft files
+            * softdir: dir to look for soft files
+            * expand_soft: whether to scan for/expand compressed soft files
+            * extract_gsm: whether to extract gsm-level soft data
+            * conv_json: whether to convert gsm soft files to json
+            * msrap_prepare: whether to prepare gsm json files for 
+                MetaSRA-pipeline
+        Returns
+            * status (0 or 1)
+    """
+    # filter softlist on valid files
+
+    softpath = os.path.join(filesdir,softdir)
+    if expand_soft:
+        expand_soft(softdir, rmcompressed = False)
+    # extract gsm soft file metadata
+    if extract_gsm:
+        gsmsoftpath = os.path.join('recount-methylation-files','gsm_soft')
+        os.makedirs(gsmsoftpath, exist_ok = True) # mkdir noclobber
+        extract_gsm_soft(gse_soft_dir = softpath,
+            gsm_soft_dir = gsmsoftpath,
+            eq
+            )
+    # convert gsm soft to json
+    try:
+        gsmsoftlist = os.listdir(gsmsoftpath)
+    except:
+        print("Error finding GSM SOFT files. Returning...")
+        return
+    if conv_json:
+        if gsmsoftlist:
+            for gsmfile in gsmsoftlist:
+                gsm_soft2json(gsm_soft_fn=gsmfile)
+        else:
+            print('No GSM SOFT files detected, skipping JSON conversion...')
+    # get the JSON filelist
+    try:
+        jsonfilelist = os.listdir(gse_soft_dir)
+    except:
+        print("Error finding JSON files. Returning...")
+        return
+    if msrap_prepare:
+        if jsonfilelist:
+            msrap_prepare_json(gsm_json_filelist = jsonfilelist)
+        else:
+            print('No GSM JSON files detected. Skipping MSRA-p preparation...')
+    return
+
+
+""" Notes and Tutorial
+
+import pymongo
+
+client = pymongo.MongoClient('localhost', 27017)
+cidats = client.recount_methylation.gsm.idats
+csoft = client.recount_methylation.gse.soft
+
+softlist = list(client.recount_methylation.gse.soft.find())
+
+softlist = [record for record in softlist if 'gseid' in record.keys()]
+
+gseindex = set([record['gseid'] for record in softlist])
+
+#
+
+import preprocess
+import os
+import re
+
+idir = os.path.join('recount-methylation-files','idats')
+sdir = os.path.join('recount-methylation-files','gse_soft')
+
+pfd = preprocess.get_mongofiles_for_preprocessing(idatsdir=idir,
+softdir = sdir)
+
+# filter on valid files
+
+
+pfd['idats'][list(pfd['idats'].keys())[0]]
+
+"""
--- src/process_soft.py
+++ src/process_soft.py
@@ -1,11 +1,14 @@
 import os
+import sys
 import re
 import gzip
 import shutil
 import subprocess
+sys.path.insert(0, os.path.join("recount-methylation-server","src"))
+from server import get_queryfilt_dict
 from dl import gettime_ntp
 
-def expand_soft(soft_dir='soft',rmcompressed=True):
+def expand_soft(soft_dir='gse_soft',rmcompressed=False):
     """ Automatically expand detected soft files
         Arguments
             * soft_dir : directory to search for soft files
@@ -38,8 +41,8 @@ def expand_soft(soft_dir='soft',rmcompressed=True):
         return 
     return [softl_compressed,statuslist]
 
-def extract_gsm_soft(gse_soft_dir='gse_soft',gsm_soft_dir='gsm_soft',
-    temp_dir='temp',edir_filt,validate=True):
+def extract_gsm_soft(gse_soft_dir = 'gse_soft',gsm_soft_dir = 'gsm_soft',
+    temp_dir = 'temp', timestamp = gettime_ntp()):
     """ Extract GSM soft file sections from GSE soft file
         Arguments 
             * gse_soft_dir : directory to search for gse soft files
@@ -47,9 +50,8 @@ def extract_gsm_soft(gse_soft_dir='gse_soft',gsm_soft_dir='gsm_soft',
         Returns
             * null, generates gsm soft files as side effect
     """
-    timestamp = str(gettime_ntp())
-    r1 = re.compile(".*soft.*")
-    dirlist = os.listdir(soft_dir)
+    r1 = re.compile(".*soft$")
+    dirlist = os.listdir(gse_soft_dir)
     soft_list = list(filter(r1.match, dirlist))
     for soft in soft_list:
         openindex = []
@@ -65,6 +67,7 @@ def extract_gsm_soft(gse_soft_dir='gse_soft',gsm_soft_dir='gsm_soft',
                     openindex.append(num)
                 lsoft.append(line)
         rxgsm = re.compile('GSM[0-9]*')
+        # may add filter based on edirect query file
         for num, openi in enumerate(openindex,0):
             # read gsm lines
             gsmsoftlines = lsoft[openi:closeindex[num]]
@@ -78,17 +81,29 @@ def extract_gsm_soft(gse_soft_dir='gse_soft',gsm_soft_dir='gsm_soft',
                 gsmfile.close()
     return
 
-def gsm_soft2json(gsm_soft_fn,gsm_json_destdir='gsm_json'):
+def gsm_soft2json(gsm_soft_fn, 
+    scriptpath = os.path.join('recount-methylation-server','src','soft2json.R'), 
+    gsmsoft_dir = 'gsm_soft',
+    filesdir = 'recount-methylation-files',
+    gsm_json_destdir='gsm_json'):
     """ Convert GSM soft file to JSON format
         Call R script to process GSM soft (XML) to JSON format
         Arguments
             * gsm_soft_fn : name of GSM soft file to process
+            * scriptpath : path to R script for JSON conversion
             * gsm_json_destdir : name of dest dir for new JSON files
         Returns
             * null, or error
     """
+    gsmsoftpath = os.path.join(filesdir,gsmsoft_dir)
+    gsmi_softpath = os.path.join(gsmsoftpath,gsm_soft_fn)
+    jsonpath = os.path.join(filesdir,gsm_json_destdir)
+    os.makedirs(jsonpath, exist_ok = True)
     try:
-        cmdlist = ['Rscript','soft2json.R',gsm_soft_fn,gsm_json_destdir]
+        if scriptpath:
+            cmdlist = ['Rscript',scriptpath,gsm_soft_fn,jsonpath]
+        else:
+            cmdlist = ['Rscript','soft2json.R',gsm_soft_fn,jsonpath]
         subprocess.call(cmdlist,shell=False)
         #subprocess.check_call(cmdlist,executable='/bin/bash')
     except subprocess.CalledProcessError as e:
