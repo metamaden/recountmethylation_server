3becb075f8906dcdae76eb216bb09087591a50de
Sean <maden@ohsu.edu>
2019-01-10 13:06:49 -0800

    Improves json formation
    Improves json formation from gsm soft files with nested terms by unnesting and appending new valid terms where found. This should greatly improve mappability with MetaSRA-pipeline.

--- src/soft2json.R
+++ src/soft2json.R
@@ -14,6 +14,26 @@ soft2json <- function(gsmsoft_fn, gsm_softpath, gsm_json_destdir){
   dffile <- as.data.frame(matrix(nrow=1,ncol=nrow(linefile)))
   colnames(dffile) <- gsub(" =.*","",linefile[,1])
   dffile[1,] <- gsub("!.*= ","",linefile[,1])
+  # scan for nested metadata and expand into new appended variables
+  for(c in 1:nrow(linefile)){
+    linec = as.character(linefile[c,])
+    if(grepl(".*:.*",linec)){
+      linecsplitlist = unlist(strsplit(linec,":"))
+      if(length(linecsplitlist)==2){
+        linecsplitlist[1] = gsub("^.*= ","",linecsplitlist[1])
+        linecsplitlist[1] = gsub("^ | $","",linecsplitlist[1])
+        linecsplitlist[1] = gsub(" ","_",linecsplitlist[1])
+        linecsplitlist[2] = gsub("^ | $","",linecsplitlist[2])
+        linecsplitlist[2] = gsub(" ","_",linecsplitlist[2])
+        if(!(grepl("^http$|^https$|^ftp$",linecsplitlist[1])) & 
+           nchar(as.character(linecsplitlist[2]))<20){
+          newcval = as.vector(gsub(".*:","",linecsplitlist[2]))
+          names(newcval) <- linecsplitlist[1]
+          dffile <- cbind(dffile,t(as.matrix(newcval)))
+        }
+      }
+    }
+  }
   jsoni <- toJSON(dffile, pretty=T)
   write(jsoni, file = file.path(gsm_json_destdir,paste0(gsmsoft_fn,".json")))
 }
