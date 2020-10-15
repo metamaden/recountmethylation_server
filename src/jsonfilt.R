cf6d4879987572be2a186a73d818b98a768e6ff3
Sean <maden@ohsu.edu>
2020-10-15 15:37:53 -0700

    Update jsonfilt.R
    updates comments

--- src/jsonfilt.R
+++ src/jsonfilt.R
@@ -1,49 +1,56 @@
 #!/usr/bin/env R
 
-# Authors: Sean Maden, Abhi Nellore
-# Filter the sample/GSM metadata JSON files, removing any
-# study/GSE record-specific data, prior to mapping with the
-# MetaSRA-pipeline.
+# Author: Sean Maden
+# This script shows how JSON-formatted sample/GSM metadata, 
+# extracted from GSE SOFT files, was filtered prior to running in
+# MetaSRA-pipeline. Most likely/highest-confidence sample type 
+# predictions from this run were stored under the "sampletype"
+# column in the main metadata table (Table S1).
 
 library(readr)
 library(jsonlite)
 
-# preamable, setting up shared variables
-syssep = "/"
-readpath = "gsm_json"
-destpath = "gsm_json_filt"
-filestem = ".json.filt"
+#------------------------
+# assign dir's and path's
+#------------------------
+# input dir/path
+json.dn <- "gsm_json"
+readpath <- file.path("recount-methylation-files", json.dn)
 
-lf.json <- list.files("gsm_json")
-keys.list <- c("!Sample_characteristics_ch1",
-               "!Sample_source_name_ch1",
+# output dir/path
+jfilt.dn <- json.dn # paste0(json.dn, "_filt")
+destpath <- file.path("recount-methylation-files", jfilt.dn)
+if(!dir.exists(destpath)){dir.create(destpath)}
+
+# new files stem
+filestem <- ".json.filt"
+
+# keys of interest
+keys.list <- c("!Sample_characteristics_ch1", "!Sample_source_name_ch1", 
                "!Sample_title")
 
-# read json data, write filtered json data
-for(i in 1:length(lf.json)){
+#------------------
+# filter json files
+#------------------
+lf.json <- list.files(readpath)
+# write filtered data as new json files
+for(i in seq(length(lf.json))){
   fni <- lf.json[i]
-  gsmi <- unlist(strsplit(fni,"\\."))[2]
-  writefn <- paste0(gsmi, filestem, collapse=".")
-  writepath <- paste0(destpath, syssep, writefn,collapse="")
-  rjsoni <- jsonlite::fromJSON(paste0(readpath,syssep,fni)) # read in json file
-  
+  ts <- unlist(strsplit(fni,"\\."))[1] # timestamp
+  gsmi <- unlist(strsplit(fni,"\\."))[2] # gsm id
+  writefn <- paste0(ts, gsmi, filestem, collapse = ".")
+  writepath <- file.path(destpath, writefn)
+  rjsoni <- jsonlite::fromJSON(file.path(readpath,fni))
   # filter on valid sample-specific keys
   message("filtering keys for file ",i)
-  rjsoni.keys <- colnames(rjsoni)
-  rf <- list()
-  for(k in 1:length(keys.list)){
-    rekf <- rjsoni.keys[grepl(keys.list[k],rjsoni.keys)]
-    for(f in 1:length(rekf)){
-      rf[[rekf[f]]] <- as.character(unlist(rjsoni[rekf[f]]))
-    }
+  rjsoni.keys <- colnames(rjsoni); rf <- list()
+  for(k in keys.list){rekf <- rjsoni.keys[grepl(k, rjsoni.keys)]
+    for(f in rekf){rf[[f]] <- as.character(unlist(rjsoni[f]))}
   }
-  
-  # write properly formatted json with OUTSIDE BRACKETS (required!)
+  # write formatted json with top and bottom outside brackets
   message("writing filtered json data for file ",i)
   jsoni <- jsonlite::toJSON(rf, pretty=T, auto_unbox = T)
-  write_lines("[", writepath)
-  write_lines(jsoni, writepath, append=T)
+  write_lines("[", writepath); write_lines(jsoni, writepath, append=T)
   write_lines("]", writepath, append=T)
-  
   message("finished file ",i)
 }
