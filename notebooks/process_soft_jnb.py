f1bc7dd31ae954e5de39837266961a3dc2cf9f16
Sean <maden@ohsu.edu>
2020-12-08 11:00:25 -0800

    Update process_soft_jnb.py

--- notebooks/process_soft_jnb.py
+++ notebooks/process_soft_jnb.py
@@ -66,8 +66,7 @@ gsm_soft files directory:
 process_soft.extract_gsm_soft(gse_softpath = settings.gsesoftpath, 
     gsm_softpath = settings.gsmsoftpath, gsmsoft_destpath = settings.gsmsoftpath)
 
-extract_gsm_soft(gse_softpath = settings.gsesoftpath, 
-    gsm_softpath = settings.gsmsoftpath, 
+extract_gsm_soft(gse_softpath = settings.gsesoftpath, gsm_softpath = settings.gsmsoftpath, 
     gsmsoft_destpath = settings.gsmsoftpath)
 
 
@@ -81,7 +80,7 @@ vall = [v for v in eqfiltdict.values()]
 keyl = [k for k in eqfiltdict.keys()]
 for i, val in enumerate(vall):
     valf = [v for v in val if not v in gsmidfilt]
-    if len(valf) > 0:
+    if len(valf) > 10:
         gseid_processnew.append(keyl[i])
     print(str(i))
 
@@ -192,12 +191,338 @@ best on your system.
 
 """
 
+
+# OLD METHOD
+
 gsm_jsonpath = os.path.join('recount-methylation-files','gsm_json_filt')
 process_soft.msrap_launchproc(nprocsamp=100, nmaxproc=5, timelim=1000000, 
     fnpatt = '.*json.filt$', gsmjsonpath = gsm_jsonpath)
 
-
-
 msrap_launchproc(nprocsamp=50, nmaxproc=20, timelim=2800, 
     fnpatt = '.*json\\.filt$', gsmjsonpath = jsonfilt_path)
+
+
+# NEW METHOD
+
+cd recount-methylation-hm450k/
+python3
+
+
+import os
+import sys
+import re
+import gzip
+import shutil
+import subprocess
+import filecmp
+import tempfile
+import pickle
+from datetime import datetime
+import time
+from random import shuffle
+sys.path.insert(0, os.path.join("recount-methylation-server","src"))
+from utilities import gettime_ntp, getlatest_filepath, get_queryfilt_dict
+from utilities import monitor_processes
+import settings
+settings.init()
+
+import subprocess
+import glob
+import sys
+import os
+import re
+sys.path.insert(0, os.path.join("recount-methylation-server","src"))
+import edirect_query
+from edirect_query import gsm_query, gse_query, gsequery_filter  
+from utilities import gettime_ntp, getlatest_filepath, querydict
+from utilities import get_queryfilt_dict  
+import settings
+settings.init()
+
+import process_soft
+
+
+
+def msrap_getsamples(json_flist=[], fnpatt=".*json.filt$", 
+    gsmjsonpath=os.path.join("recount-methylation-files", "gsm_json_filt"), 
+    nprocsamp=50, nmaxproc=20):
+    """ msrap_getsamples
+        
+        Get the validated samples file list
+
+        Arguments:
+            * json_flist (list) : List of GSM JSON filenames to process. If not 
+                provided, function automatically detects any new GSM JSON files
+                without available MetaSRA-pipeline outfiles.
+            * fnpatt (str): Filename pattern of valid json files to identify.
+            * gsmjsonpath (path): Path to JSON formatted sample SOFT data.
+            * nprocsamp (int) : Number of samples to process per screen deployed.
+            * nmaxproc (int) : Maximum processes to launch
+            * timelim (int) : time limit (minutes) for monitoring processes.
+            * statint (int) : time (seconds) to sleep before next status update.
+        Returns:
+            (Null) Generates >=1 processes for file sublists
+
+    """
+    print("Checking dirs for msrapout and msrap logs...")
+    os.makedirs(settings.gsmmsrapoutpath, exist_ok=True)
+    os.makedirs(settings.msraplogspath, exist_ok=True)
+    # detect gsm soft files
+    psoftpath = settings.psoftscriptpath
+    if os.path.exists(psoftpath):
+        print("Process soft script found at: "+str(psoftpath))
+    gsmsoftpath = settings.gsmsoftpath
+    gsmmsrapoutpath = settings.gsmmsrapoutpath
+    jsonfnpattern = fnpatt
+    rjson = re.compile(jsonfnpattern)
+    msrapoutfnpattern = settings.msrapoutfnpattern
+    rmsrapout = re.compile(msrapoutfnpattern)
+    # generate fl list of valid json files that haven't been processed yet
+    fl = []
+    if json_flist and len(json_flist)>0:
+        jsonfnlist = list(filter(rjson.match, json_flist)) 
+        jsongsmlist = [x.split('.')[1] for x in jsonfnlist]
+    else:
+        json_flist = os.listdir(gsmjsonpath)
+        jsonfnlist = list(filter(rjson.match, json_flist)) 
+        jsongsmlist = [x.split('.')[1] for x in jsonfnlist]
+    msrapoutfnlist = os.listdir(gsmmsrapoutpath) 
+    msrapoutfnlist = list(filter(rmsrapout.match, msrapoutfnlist))
+    print("Found "+str(len(msrapoutfnlist))+" files with pattern "
+        +msrapoutfnpattern+". Continuing...")
+    msrapgsmlist = [x.split('.')[2] for x in msrapoutfnlist]
+    gsmprocess = [g for g in jsongsmlist 
+            if not g in msrapgsmlist and
+            g[0:3]=='GSM'
+        ]
+    for index, gsmid in enumerate(gsmprocess):
+        gjsonfpath = getlatest_filepath(filepath=gsmjsonpath,
+                filestr=gsmid, embeddedpattern=True, tslocindex=0,
+                returntype='returnlist'
+            )
+        if gjsonfpath and len(gjsonfpath)==1:
+            gjsonfn = [os.path.basename(gjsonfpath[0])]
+        else:
+            gjsonfn = [os.path.basename(fn) for fn in gjsonfpath]
+        gjsonfn = gjsonfn[0]
+        fl.append(gjsonfn)
+        numi = 100*(index/len(gsmprocess))
+        perci = str(round(numi,2))
+        print("Appended file "+gjsonfn+" to files list to process. "
+            +"Progress: "+str(index)+"/"+str(len(gsmprocess))+"="
+            +perci+"%. Continuing...")
+    # form list of fn lists based on nscreensi and indices/slices
+    if fl:
+        print("Forming list of fn lists for screen deployment...")
+        ll = []
+        rangelist = [i for i in range(0, len(fl), nprocsamp)]
+        for enum, i in enumerate(rangelist[:-1]):
+            ll.append(fl[i:rangelist[enum+1]])
+        if len(fl[rangelist[-1]::]) > 0:
+            ll.append(fl[rangelist[-1]::])
+    else:
+        print("Error, no files list object to process. Returning...")
+        return None
+    print('screens ll list, len = ' + str(len(ll)))
+    print('nmax screens = '+str(nmaxproc))
+    return ll
+
+
+
+
+
+
+
+ll = msrap_getsamples(nprocsamp=2100, nmaxproc=22)
+
+
+
+
+
+lall = ll
+for ll in lall:
+    json_flist=ll
+    jsonpatt=".*json.filt$"
+    gsm_jsonpath = os.path.join("recount-methylation-files", "gsm_json_filt")
+    timestamp=gettime_ntp()
+    print("Validating files on GSM equery results...")
+    eqfiltdict=get_queryfilt_dict()
+    validgsmlist = [gsmid for gselist in list(eqfiltdict.values()) 
+        for gsmid in gselist
+    ]
+    msrap_runpath = settings.msraprunscriptpath
+    # if filenames provided, form list, else list json dir contents
+    print("Validating JSON files on filename patterns...")
+    if json_flist and len(json_flist)>0:
+        rjson = re.compile(jsonpatt)
+        gsm_json_fn_list = list(filter(rjson.match, json_flist)) 
+    else:
+        gsm_json_fn_list = os.listdir(gsm_jsonpath)
+        rjson = re.compile(jsonpatt)
+        gsm_json_fn_list = list(filter(rjson.match, gsm_json_fn_list))
+    # make write path
+    msrap_destpath = settings.gsmmsrapoutpath
+    os.makedirs(msrap_destpath, exist_ok=True)
+    print("Getting read and write paths...")
+    # define file read paths
+    gsm_read_pathl = [os.path.join(gsm_jsonpath, fn) for fn in gsm_json_fn_list]
+    # define write paths, same file order as read paths
+    msrap_fn = settings.msrapfnstem
+    ts_gsm_fnl = [".".join(fn.split(".")[0:2]) for fn in gsm_json_fn_list]
+    gsm_write_fnl = [".".join([timestamp,outfn,msrap_fn]) for outfn in ts_gsm_fnl]
+    gsm_write_pathl = [os.path.join(msrap_destpath,fn) for fn in gsm_write_fnl]
+    # note -- pass 1 long args list once
+    args0 = ";".join(gsm_read_pathl)
+    args1 = ";".join(gsm_write_pathl)
+    cmdlist = " ".join(["python2", msrap_runpath, "--fnvread", '"'+args0+'"', "--fnvwrite", '"'+args1+'"'])
+    print("Calling the subprocess...")
+    proc = subprocess.call(cmdlist, shell=True)
+    print("Finished calling subprocess. Returning...") 
+
+
+
+#-----
+# old
+#-----
+
+
+
+import os
+import sys
+import re
+import gzip
+import shutil
+import subprocess
+import filecmp
+import tempfile
+import pickle
+from datetime import datetime
+import time
+from random import shuffle
+sys.path.insert(0, os.path.join("recount-methylation-server","src"))
+from utilities import gettime_ntp, getlatest_filepath, get_queryfilt_dict
+from utilities import monitor_processes
+import settings
+settings.init()
+
+json_flist=[]
+fnpatt='.*json.filt$'
+gsmjsonpath = os.path.join('recount-methylation-files','gsm_json_filt')
+timestamp=gettime_ntp()
+nprocsamp=500
+nmaxproc=15
+timelim=2800
+statint=5
+
+if not os.path.exists(settings.msraprunscriptpath):
+        print("Error: MetaSRA-pipeline script not found. Please check your "
+            +"local download of MetaSRA-pipeline.")
+        return None
+print("Checking dirs for msrapout and msrap logs...")
+os.makedirs(settings.gsmmsrapoutpath, exist_ok=True)
+os.makedirs(settings.msraplogspath, exist_ok=True)
+# detect gsm soft files
+psoftpath = settings.psoftscriptpath
+if os.path.exists(psoftpath):
+    print("Process soft script found at: "+str(psoftpath))
+gsmsoftpath = settings.gsmsoftpath
+gsmmsrapoutpath = settings.gsmmsrapoutpath
+jsonfnpattern = fnpatt
+rjson = re.compile(jsonfnpattern)
+msrapoutfnpattern = settings.msrapoutfnpattern
+rmsrapout = re.compile(msrapoutfnpattern)
+# generate fl list of valid json files that haven't been processed yet
+fl = []
+if json_flist and len(json_flist)>0:
+    jsonfnlist = list(filter(rjson.match, json_flist)) 
+    jsongsmlist = [x.split('.')[1] for x in jsonfnlist]
+else:
+    json_flist = os.listdir(gsmjsonpath)
+    jsonfnlist = list(filter(rjson.match, json_flist)) 
+    jsongsmlist = [x.split('.')[1] for x in jsonfnlist]
+msrapoutfnlist = os.listdir(gsmmsrapoutpath) 
+msrapoutfnlist = list(filter(rmsrapout.match, msrapoutfnlist))
+print("Found "+str(len(msrapoutfnlist))+" files with pattern "
+    +msrapoutfnpattern+". Continuing...")
+msrapgsmlist = [x.split('.')[2] for x in msrapoutfnlist]
+gsmprocess = [g for g in jsongsmlist 
+        if not g in msrapgsmlist and
+        g[0:3]=='GSM'
+    ]
+for index, gsmid in enumerate(gsmprocess):
+    gjsonfpath = getlatest_filepath(filepath=gsmjsonpath,
+            filestr=gsmid, embeddedpattern=True, tslocindex=0,
+            returntype='returnlist'
+        )
+    if gjsonfpath and len(gjsonfpath)==1:
+        gjsonfn = [os.path.basename(gjsonfpath[0])]
+    else:
+        gjsonfn = [os.path.basename(fn) for fn in gjsonfpath]
+    gjsonfn = gjsonfn[0]
+    fl.append(gjsonfn)
+    numi = 100*(index/len(gsmprocess))
+    perci = str(round(numi,2))
+    print("Appended file "+gjsonfn+" to files list to process. "
+        +"Progress: "+str(index)+"/"+str(len(gsmprocess))+"="
+        +perci+"%. Continuing...")
+# form list of fn lists based on nscreensi and indices/slices
+if fl:
+    print("Forming list of fn lists for screen deployment...")
+    ll = []
+    rangelist = [i for i in range(0, len(fl), nprocsamp)]
+    for enum, i in enumerate(rangelist[:-1]):
+        ll.append(fl[i:rangelist[enum+1]])
+    if len(fl[rangelist[-1]::]) > 0:
+        ll.append(fl[rangelist[-1]::])
+else:
+    print("Error, no files list object to process. Returning...")
+    return None
+print('screens ll list, len = ' + str(len(ll)))
+print('nmax screens = '+str(nmaxproc))
+
+ll = ll[0:nmaxproc] # slice ll based on screen count max
+ts = timestamp # single timestamp call shared across screens
+
+# parse a single session
+lall = ll
+for ll in lall:
+    json_flist=ll
+    jsonpatt=".*json.filt$"
+    gsm_jsonpath = os.path.join("recount-methylation-files", "gsm_json_filt")
+    timestamp=gettime_ntp()
+    print("Validating files on GSM equery results...")
+    eqfiltdict=get_queryfilt_dict()
+    validgsmlist = [gsmid for gselist in list(eqfiltdict.values()) 
+        for gsmid in gselist
+    ]
+    msrap_runpath = settings.msraprunscriptpath
+    # if filenames provided, form list, else list json dir contents
+    print("Validating JSON files on filename patterns...")
+    if json_flist and len(json_flist)>0:
+        rjson = re.compile(jsonpatt)
+        gsm_json_fn_list = list(filter(rjson.match, json_flist)) 
+    else:
+        gsm_json_fn_list = os.listdir(gsm_jsonpath)
+        rjson = re.compile(jsonpatt)
+        gsm_json_fn_list = list(filter(rjson.match, gsm_json_fn_list))
+    # make write path
+    msrap_destpath = settings.gsmmsrapoutpath
+    os.makedirs(msrap_destpath, exist_ok=True)
+    print("Getting read and write paths...")
+    # define file read paths
+    gsm_read_pathl = [os.path.join(gsm_jsonpath, fn) for fn in gsm_json_fn_list]
+    # define write paths, same file order as read paths
+    msrap_fn = settings.msrapfnstem
+    ts_gsm_fnl = [".".join(fn.split(".")[0:2]) for fn in gsm_json_fn_list]
+    gsm_write_fnl = [".".join([timestamp,outfn,msrap_fn]) for outfn in ts_gsm_fnl]
+    gsm_write_pathl = [os.path.join(msrap_destpath,fn) for fn in gsm_write_fnl]
+    # note -- pass 1 long args list once
+    args0 = ";".join(gsm_read_pathl)
+    args1 = ";".join(gsm_write_pathl)
+    cmdlist = " ".join(["python2", msrap_runpath, "--fnvread", '"'+args0+'"', "--fnvwrite", '"'+args1+'"'])
+    print("Calling the subprocess...")
+    proc = subprocess.call(cmdlist, shell=True)
+    print("Finished calling subprocess. Returning...")    
+
+
 
