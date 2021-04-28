9cd85dac43f24bd528a4b5328861bce3b915b92d
Sean <maden@ohsu.edu>
2021-04-28 13:15:44 -0700

    Update process_idats.py

--- src/process_idats.py
+++ src/process_idats.py
@@ -11,8 +11,7 @@
 
 import os, sys, re, gzip, shutil
 sys.path.insert(0, os.path.join("recountmethylation_server","src"))
-import settings
-settings.init()
+import settings; settings.init()
 
 def expand_idats(idatspath, compext = ".*idat.gz$", expext = ".*idat$"):
     """ Detect and expand available idat files.
@@ -39,22 +38,14 @@ def expand_idats(idatspath, compext = ".*idat.gz$", expext = ".*idat$"):
     print("Expanding "+str(len(idats_fnlist_filt))+"compressed IDATs...")
     for compidat in idats_fnlist_filt:
         idat_fn = os.path.splitext(compidat)[0]
-        statuslist = []
-        ridatd[compidat] = []
+        statuslist = []; ridatd[compidat] = []
         with gzip.open(os.path.join(idatspath, compidat), 'rb') as f_in:
             with open(os.path.join(idatspath, idat_fn), 'wb') as f_out:
                 try:
-                    shutil.copyfileobj(f_in, f_out)
-                    ridatd[compidat].append(1)
+                    shutil.copyfileobj(f_in, f_out); ridatd[compidat].append(1)
                 except shutil.Error as se:
                     ridatd[compidat].append(se)
     return ridatd
-
-def cleanup_idats(remove_type = "expanded", expext = ".*idat$", 
-    compext = ".*idat.gz$"):
-    """ Remove redundant IDATs 
-    """
-    return True
 
 if __name__ == "__main__":
     """ Process downloaded IDATs
