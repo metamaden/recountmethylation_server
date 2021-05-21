ca8a6e60d0c272b095c43d2ec9bf42afa61e2953
Sean <maden@ohsu.edu>
2021-05-21 14:56:55 -0700

    Update rsheet.py

--- src/rsheet.py
+++ src/rsheet.py
@@ -24,10 +24,10 @@ def new_idat_hlinks(gsmid, ts, igrn_fn, ired_fn):
         Make new hlink files and return new hlink paths.
 
         Arguments
-        * gsmid : Valid sample GSM ID (str).
-        * ts : Timestamp (int or str).
-        * igrn_fn : Filename of green intensity data file (IDAT, str).
-        * ired_fn : Filename of red intensity data file (IDAT, str).
+        * gsmid: Valid sample GSM ID (str).
+        * ts: Timestamp (int or str).
+        * igrn_fn: Filename of green intensity data file (IDAT, str).
+        * ired_fn: Filename of red intensity data file (IDAT, str).
         
         Returns
         * rlist (list): List of path to new grn [0] and red [1] hlinked idats
@@ -52,20 +52,216 @@ def new_idat_hlinks(gsmid, ts, igrn_fn, ired_fn):
         )
     rlist.append(os.path.join(settings.idatspath, gnewhlinkfn))
     rlist.append(os.path.join(settings.idatspath, rnewhlinkfn))
-    return(rlist)
+    return rlist
 
-def rmdb_fpaths(rmhlinks=False):
+def rmdb_fpaths(mdb_host = settings.rmdbhost, mdb_port = settings.rmdbport):
     """ rmdb_fpaths
 
         Get filepaths for existant sample idats and msrap outfiles.
 
         Arguments:
-        * rmhlinks : Whether to remove old hardlinks and form new ones, 
-                regardless of whether current hlinks exist (boolean).
+        * mdb_host: Host name for the instance (setting param, str).
+        * mdb_port: Port number for the instance (setting param, int).
 
         Returns:
-        * gsmdocdict (dict.) : Dictionary of validated filepaths.
+        * hlinklist, list of new hlink files created at settings.idatspath.
 
+    """
+    timestamp = gettime_ntp()
+    # connect to RMDB mongodb
+    client = pymongo.MongoClient(mdb_host, mdb_port)
+    mdbcon = client.recount_methylation; mdb_idatscon = mdbcon.gsm.idats
+    mdb_idatrecords = list(mdb_idatscon.find())
+    # list all previously expanded idat files directy from idats dir
+    instpath_allidats = os.listdir(settings.idatspath)
+    # compressed idats
+    instpath_compidats = list(filter(re.compile('.*\.idat.gz$').match, 
+        instpath_allidats))
+    # idat hlinks
+    instpath_hlink = list(filter(re.compile('.*hlink.*').match, 
+        instpath_allidats))
+    # expanded idats without hlinks
+    instpath_nohlink = list(filter(re.compile('.*\.idat$').match, 
+        instpath_allidats))
+    instpath_nohlink = [i for i in instpath_nohlink 
+        if not i in instpath_hlink]
+    print("Detected " +str(len(instpath_compidats))+ 
+        " compressed IDATs, " + str(len(instpath_nohlink)) + 
+        " expanded IDATs without hlinks, and " +
+        str(len(instpath_hlink)) + " expanded IDATs with hlinks.")
+    print("Getting GSM IDs for IDATs without hlinks...")
+    instpath_nohlink_gsm =  list(set([i.split(".")[0] for i in instpath_nohlink]))
+    print("Getting IDAT filepaths from MongoDB records, "+
+        "for GSM IDs lacking hlinks...")
+    mdb_nohlink_gsmlist = list(set([i["gsmid"] for i in mdb_idatrecords 
+        if i["gsmid"] in instpath_nohlink_gsm]))
+    instpath_idatspathlist = [i for i in instpath_nohlink 
+        if i.split(".")[0] in mdb_nohlink_gsmlist]
+    hlinklist=[]
+    for gsmid in mdb_nohlink_gsmlist:
+        print("Processing GSM ID " + gsmid + "..."); 
+        ired_fn = ""; igrn_fn = ""; basename_grn = ""; basename_red = ""
+        gsm_idats = [i for i in instpath_idatspathlist
+            if i.split(".")[0] == gsmid and 
+            os.path.exists(os.path.join(settings.idatspath, i))]
+        try:
+            igrn_fn = list(filter(re.compile(".*Grn\.idat$").match, gsm_idats))[0]
+            ired_fn = list(filter(re.compile(".*Red\.idat$").match, gsm_idats))[0]
+            basename_grn = "_".join(igrn_fn.split(".")[2].split("_")[0:-1])
+            basename_red = "_".join(ired_fn.split(".")[2].split("_")[0:-1])
+            if basename_grn==basename_red and not basename_grn == "" and not basename_red == "":
+                print("Making new IDAT hlinks for GSM ID " + gsmid)
+                rlist = new_idat_hlinks(gsmid = gsmid, ts = timestamp, 
+                    igrn_fn = igrn_fn, ired_fn = ired_fn)
+                hlinklist.append(rlist)
+        except:
+            print("Couldn't find Red and Grn IDATs for GSM ID " + gsmid)
+        print("Finished with GSM ID " + gsmid)
+    print("Made " + str(len(hlinklist)) + " new IDAT hlinks. Returning...")
+    return hlinklist
+
+def compile_rsheet(gsmfpathdict):
+    """ compile_rsheet
+
+        Takes dictionary of GSM IDs. Compiles valid GSM IDs, filenames, and 
+        path into an rsheet object.
+        
+        Arguments:
+            * gsmfpathdict: gsm paths dict obj output from rmdb_fpaths() 
+                (dict).
+        
+        Returns:
+            * lsheet, produces rsheet file as a side effect.
+
+    """
+    timestamp = gettime_ntp()
+    print("Getting equery filter...")
+    eqd = get_queryfilt_dict()
+    gsmvalidlist = list(set([gsmid for gselist in list(eqd.values()) 
+        for gsmid in gselist
+    ]))
+    sheetspath = settings.sheetspath; sheetfn_ext = settings.sheetfnstem
+    os.makedirs(sheetspath, exist_ok = True)
+    sheets_fpath = os.path.join(sheetspath, ".".join([timestamp, sheetfn_ext]))
+    # table written as list of row strings
+    print("Forming table list for rsheet..."); lsheet = []
+    lsheet.append(" ".join(["gsmid",
+        "gseid",
+        "idats_fn",
+        "msrapmd_fn",
+        "msrapmd_flatjson",
+        "SENTRIX_ID",
+        "ARRAY_ID",
+        "Basename"]))
+    lsheet[0] = lsheet[0]+"\n"; print("Forming filtered GSM dictionary...")
+    gsmvalid_fpathlist = {key:value for (key,value) in gsmfpathdict.items() 
+        if key in gsmvalidlist}
+    if gsmvalid_fpathlist:
+        print("Starting iterations on gsm filepaths list of len = "
+            +str(len(list(gsmvalid_fpathlist.keys()))))
+        for gsmindex, gsmid in enumerate(gsmvalid_fpathlist, 1):
+            print("Beginning GSM num "+str(gsmindex)+", id: "+str(gsmid))
+            gsmvalid_fp = [fp for fp in gsmvalid_fpathlist[gsmid] 
+                if not fp==None
+                and not fp==False]
+            if gsmvalid_fp:
+                print("Getting GSE ID...")
+                gseid = ';'.join(list(set([gsek for gsek in list(eqd.keys()) 
+                            if gsmid in eqd[gsek]
+                            ]
+                        )      
+                    )
+                )
+                print("GSE id found: "+str(gseid))
+                gsm_fpaths = gsmvalid_fp
+                gsmi_redidatpath = [fp for fp in gsm_fpaths if "_Red.idat" in fp]
+                gsmi_grnidatpath = [fp for fp in gsm_fpaths if "_Grn.idat" in fp]
+                gsmi_msrappath = [fp for fp in gsm_fpaths if "soft.msrapout" in fp]
+                if gsmi_redidatpath and gsmi_grnidatpath:
+                    print("Found paired channel idats for GSM...")
+                    gsmi_redidatpath = gsmi_redidatpath[0]
+                    gsmi_grnidatpath = gsmi_grnidatpath[0]
+                    # idat filenames
+                    grn_idatfn = os.path.basename(gsmi_grnidatpath)
+                    red_idatfn = os.path.basename(gsmi_redidatpath)
+                    # sample basename (common stem of channel array filenames)
+                    print("Getting sample basename..."); gsmimdd = []
+                    gsmi_basename = "_".join(red_idatfn.split("_")[0:3]) # basename
+                    if gsmi_msrappath:
+                        print("Detected metadata file for GSM.")
+                        gsmi_msrappath = gsmi_msrappath[0]
+                        gsmi_msrappath_var = os.path.basename(gsmi_msrappath)   
+                        print("Forming flattened sample metadata...")
+                        try:
+                            # load msrap mapped terms json file
+                            with open(gsmi_msrappath, 'r') as msrapmd:
+                                gsmimdd = json.load(msrapmd)
+                        except json.decoder.JSONDecodeError:
+                            print("Error, cannot load non-json file: "+gsmi_msrappath)
+                            gsmi_msrappath_var = "NA"
+                    else:
+                        gsmi_msrappath_var = "NA"
+                    if gsmimdd and not gsmi_msrappath_var == "NA":
+                        gsmi_md = gsmimdd[0]; gmd = []
+                        # coerce json metadata to flat string
+                        for key in list(gsmi_md.keys()):
+                            print(str(key))
+                            kval = ''
+                            if key == 'sample type':
+                                print("key = 'sample type'")
+                                print("kval = "+str(gsmi_md[key]))
+                                gmd.append(str("sampletype="+str(gsmi_md[key])))
+                            if key == 'real-value properties':
+                                print("key = "+str(key))
+                                kval = gsmi_md[key]
+                                print("kval : "+str(kval))
+                                for index, val in enumerate(kval):
+                                    subkval = kval[index]
+                                    print("subkval : "+str(subkval))
+                                    gmd.append(str(subkval['property_id']))
+                            if key == 'mapped ontology terms':
+                                print("key = "+str(key))
+                                kval = gsmi_md[key]
+                                gmd.append(";".join([term for term in kval]))
+                            if key == 'sample-type confidence':
+                                print("key = "+str(key))
+                                gmd.append('sampletypeconf='+str(gsmi_md[key]))
+                        gsmi_mdvar = ";".join(gmd) # long metadata string
+                    else:
+                        gsmi_mdvar = "NA"     
+                    # form table row entry for gsmid as long string
+                    print("Adding row to table list...")
+                    lgsmi = " ".join([gsmid, # gsm id
+                        gseid, # gse id
+                        ";".join([red_idatfn,grn_idatfn]), # idat filenames
+                        gsmi_msrappath_var, # metadata filename
+                        gsmi_mdvar, # flattened json file
+                        grn_idatfn.split("_")[-2], # sentrix id
+                        grn_idatfn.split("_")[-3], # array id
+                        gsmi_basename # minfi path Basename, for arrays
+                    ])
+                    lgsmi = lgsmi+"\n"
+                    lsheet.append(lgsmi)
+                else:
+                    print("Error: GSM is missing one or more valid filepaths. Continuing...")
+    else:
+        print("No valid GSM IDs detected. Check idats and pipeline folders.")
+        return None
+    print("Finished processing the GSM files dictionary, writing new rsheet...")
+    with open(sheets_fpath,'+w') as fsheet:
+        for gsmitem in lsheet:
+            fsheet.write(gsmitem)
+    return lsheet
+
+
+def rmdb_fpaths_old(rmhlinks=False):
+    """ rmdb_fpaths
+        Get filepaths for existant sample idats and msrap outfiles.
+        Arguments:
+        * rmhlinks : Whether to remove old hardlinks and form new ones, 
+                regardless of whether current hlinks exist (boolean).
+        Returns:
+        * gsmdocdict (dict.) : Dictionary of validated filepaths.
     """
     timestamp = gettime_ntp()
     # connect to RMDB mongodb
@@ -214,151 +410,23 @@ def rmdb_fpaths(rmhlinks=False):
     # return gsmid dictionary with lists of filtered results or valid fpaths
     return gsm_fpaths_dd
 
-def compile_rsheet(gsmfpathdict):
-    """ compile_rsheet
-
-        Takes gsm dictionary outputted from rmdb_files_for_processing(). 
-        Compiles valid ids, filenames, and path into an rsheet object.
-        
-        Arguments:
-            * gsmfpathdict : gsm paths dict obj output from rmdb_fpaths() (dict).
-        
-        Returns:
-            * null, produces rsheet file as a side effect.
-
-    """
-    timestamp = gettime_ntp()
-    print("Getting equery filter...")
-    eqd = get_queryfilt_dict()
-    gsmvalidlist = list(set([gsmid for gselist in list(eqd.values()) 
-        for gsmid in gselist
-    ]))
-    sheetspath = settings.sheetspath; sheetfn_ext = settings.sheetfnstem
-    os.makedirs(sheetspath, exist_ok = True)
-    sheets_fpath = os.path.join(sheetspath, ".".join([timestamp, sheetfn_ext]))
-    # table written as list of row strings
-    print("Forming table list for rsheet..."); lsheet = []
-    lsheet.append(" ".join(["gsmid",
-        "gseid",
-        "idats_fn",
-        "msrapmd_fn",
-        "msrapmd_flatjson",
-        "SENTRIX_ID",
-        "ARRAY_ID",
-        "Basename"]))
-    lsheet[0] = lsheet[0]+"\n"; print("Forming filtered GSM dictionary...")
-    gsmvalid_fpathlist = {key:value for (key,value) in gsmfpathdict.items() 
-        if key in gsmvalidlist}
-    if gsmvalid_fpathlist:
-        print("Starting iterations on gsm filepaths list of len = "
-            +str(len(list(gsmvalid_fpathlist.keys()))))
-        for gsmindex, gsmid in enumerate(gsmvalid_fpathlist, 1):
-            print("Beginning GSM num "+str(gsmindex)+", id: "+str(gsmid))
-            gsmvalid_fp = [fp for fp in gsmvalid_fpathlist[gsmid] 
-                if not fp==None
-                and not fp==False]
-            if gsmvalid_fp:
-                print("Getting GSE ID...")
-                gseid = ';'.join(list(set([gsek for gsek in list(eqd.keys()) 
-                            if gsmid in eqd[gsek]
-                            ]
-                        )      
-                    )
-                )
-                print("GSE id found: "+str(gseid))
-                gsm_fpaths = gsmvalid_fp
-                gsmi_redidatpath = [fp for fp in gsm_fpaths if "_Red.idat" in fp]
-                gsmi_grnidatpath = [fp for fp in gsm_fpaths if "_Grn.idat" in fp]
-                gsmi_msrappath = [fp for fp in gsm_fpaths if "soft.msrapout" in fp]
-                if gsmi_redidatpath and gsmi_grnidatpath:
-                    print("Found paired channel idats for GSM...")
-                    gsmi_redidatpath = gsmi_redidatpath[0]
-                    gsmi_grnidatpath = gsmi_grnidatpath[0]
-                    # idat filenames
-                    grn_idatfn = os.path.basename(gsmi_grnidatpath)
-                    red_idatfn = os.path.basename(gsmi_redidatpath)
-                    # sample basename (common stem of channel array filenames)
-                    print("Getting sample basename..."); gsmimdd = []
-                    gsmi_basename = "_".join(red_idatfn.split("_")[0:3]) # basename
-                    if gsmi_msrappath:
-                        print("Detected metadata file for GSM.")
-                        gsmi_msrappath = gsmi_msrappath[0]
-                        gsmi_msrappath_var = os.path.basename(gsmi_msrappath)   
-                        print("Forming flattened sample metadata...")
-                        try:
-                            # load msrap mapped terms json file
-                            with open(gsmi_msrappath, 'r') as msrapmd:
-                                gsmimdd = json.load(msrapmd)
-                        except json.decoder.JSONDecodeError:
-                            print("Error, cannot load non-json file: "+gsmi_msrappath)
-                            gsmi_msrappath_var = "NA"
-                    else:
-                        gsmi_msrappath_var = "NA"
-                    if gsmimdd and not gsmi_msrappath_var == "NA":
-                        gsmi_md = gsmimdd[0]; gmd = []
-                        # coerce json metadata to flat string
-                        for key in list(gsmi_md.keys()):
-                            print(str(key))
-                            kval = ''
-                            if key == 'sample type':
-                                print("key = 'sample type'")
-                                print("kval = "+str(gsmi_md[key]))
-                                gmd.append(str("sampletype="+str(gsmi_md[key])))
-                            if key == 'real-value properties':
-                                print("key = "+str(key))
-                                kval = gsmi_md[key]
-                                print("kval : "+str(kval))
-                                for index, val in enumerate(kval):
-                                    subkval = kval[index]
-                                    print("subkval : "+str(subkval))
-                                    gmd.append(str(subkval['property_id']))
-                            if key == 'mapped ontology terms':
-                                print("key = "+str(key))
-                                kval = gsmi_md[key]
-                                gmd.append(";".join([term for term in kval]))
-                            if key == 'sample-type confidence':
-                                print("key = "+str(key))
-                                gmd.append('sampletypeconf='+str(gsmi_md[key]))
-                        gsmi_mdvar = ";".join(gmd) # long metadata string
-                    else:
-                        gsmi_mdvar = "NA"     
-                    # form table row entry for gsmid as long string
-                    print("Adding row to table list...")
-                    lgsmi = " ".join([gsmid, # gsm id
-                        gseid, # gse id
-                        ";".join([red_idatfn,grn_idatfn]), # idat filenames
-                        gsmi_msrappath_var, # metadata filename
-                        gsmi_mdvar, # flattened json file
-                        grn_idatfn.split("_")[-2], # sentrix id
-                        grn_idatfn.split("_")[-3], # array id
-                        gsmi_basename # minfi path Basename, for arrays
-                    ])
-                    lgsmi = lgsmi+"\n"
-                    lsheet.append(lgsmi)
-                else:
-                    print("Error: GSM is missing one or more valid filepaths. Continuing...")
-    else:
-        print("No valid GSM IDs detected. Check idats and pipeline folders.")
-        return None
-    print("Finished processing the GSM files dictionary, writing new rsheet...")
-    with open(sheets_fpath,'+w') as fsheet:
-        for gsmitem in lsheet:
-            fsheet.write(gsmitem)
-    return lsheet
-
 if __name__ == "__main__":
     """ rsheet.py
 
-        Coordinate MongoDB documents on valid IDATs and SOFT files.
+        Coordinate MongoDB documents on valid IDATs and SOFT files. First 
+        generates any missing hlink files, then passes to compile_rsheet a 
+        dictionary of GSM IDs corresponding to newlt created hlink files for
+        validation.
 
     """
     try:
-        gsmfpd = rmdb_fpaths()
-        if gsmfpd:
-            print("Successfully ran rmdb_files_for_processing(). Continuing...")
-            rsheet = compile_rsheet(gsmfpd)
-            if rsheet:
-                print("Successfully ran compile_rsheet() on new gsmdd dict.")
-        print("Completed all tasks!")
+        hlinklist = rmdb_fpaths()
+        gsmlist = [os.path.basename(i[0]).split(".")[0] for i in hlinklist] 
+        #if gsmfpd:
+        #    print("Successfully ran rmdb_files_for_processing(). Continuing...")
+        #    rsheet = compile_rsheet(gsmdict)
+        #    if rsheet:
+        #        print("Successfully ran compile_rsheet() on new gsmdd dict.")
+        #print("Completed all tasks!")
     except: 
         print("Error running rsheet.py! Processes not completed.")
